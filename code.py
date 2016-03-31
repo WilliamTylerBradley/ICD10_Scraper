96cf564647afea51e23a0243651fe316a8bc75d0
williamtylerbradley <william.tyler.bradley@gmail.com>
2016-03-30 21:14:03 -0400

    Mostly Finished

--- code.py
+++ code.py
@@ -10,21 +10,15 @@ icd10_codes = {}
 
 # Function to pull links
 def follow_links(url):
-	#print(url)
 	page = requests.get("http://www.icd10data.com"+url)
 	soup = BeautifulSoup(page.text)
 	next_urls = soup.find_all('a', href=True)
 	no_matches = True
 	for a in next_urls:
-		#print a['href'][:len(url)]
-		if a['href'][:len(url)] == url and len(a['href']) > len(url):
-			#print(a['href'])
+		## the -1 on the lengths is for urls with missing '-', check M84.x
+		if a['href'][:(len(url)-1)] == url[:(len(url)-1)] and a['href'].count('/') > url.count('/'):
 			follow_links(a['href'])
 			no_matches = False
-		#else:
-			#print("next url: ",a['href'])
-			#print("match url ",a['href'][:(len(url)+1)])
-			#print("len url ",len(a['href']))
 	if no_matches:
 		icd_10_cd = url.split('/')
 		icd_10_cd = icd_10_cd[len(icd_10_cd)-1]
@@ -33,16 +27,13 @@ def follow_links(url):
 			for h2 in h2_list:
 				print icd_10_cd+' '+h2.text
 				icd10_codes[icd_10_cd] = h2.text
-				#writer.writerow((icd_10_cd,h2.text))
 		
 
 # http://www.icd10data.com/ICD10CM/Codes
-#url = "/ICD10CM/Codes"
+url = "/ICD10CM/Codes"
 
-url = "/ICD10CM/Codes/A00-B99"
-
-
-
+#url = "/ICD10CM/Codes/M00-M99/M80-M85/M84-"
+#url = "/ICD10CM/Codes/C00-D49/C7A-C7A/C7A-"
 #page = requests.get(url)
 #soup = BeautifulSoup(page.text)
 
@@ -56,5 +47,6 @@ with open("ICD10_Codes.csv",'wb') as file:
 	for code, text in icd10_codes.items():
 		try:
 			writer.writerow([code, text.encode('latin-1')])
+			print('Done')
 		except:
 			print(text)
