0ebc0cd28742c890890d3104b86b301596b116bd
williamtylerbradley <william.tyler.bradley@gmail.com>
2016-05-14 21:27:59 -0400

    Commented it up
    So I can understand what I was doing later

--- code.py
+++ code.py
@@ -1,24 +1,42 @@
+##########
+# This code scrapes ICD 10 data from http://www.icd10data.com
+##########
+
+## Imports
 import csv
 import requests
 import re
 from bs4 import BeautifulSoup
 
+# Start blank dictionary
 icd10_codes = {}
 
-#file = open("ICD10_Codes.csv",'wb')
-#writer = csv.writer(file)
-
-# Function to pull links
+## Function to pull links
+# Becuase of the site's layout as a tree structure, using a recursive function makes sense
+# This function looks through all the links on a given page
+# If any are further down the tree, follow them
+# If none are further down the tree, you reached a final ICD10 code, save the data
 def follow_links(url):
+	# Get the page and find all the links
 	page = requests.get("http://www.icd10data.com"+url)
 	soup = BeautifulSoup(page.text)
 	next_urls = soup.find_all('a', href=True)
+	# Assume you are on a final page for a diagnosis code by
+	#	setting a variable for finding new links to false
 	no_matches = True
+	# Look through the links
 	for a in next_urls:
-		## the -1 on the lengths is for urls with missing '-', check M84.x
+		# If any match the current page and have a little more (another '/'), then it is further down the tree
+		# Ex: 'http://www.icd10data.com/ICD10CM/Codes/S00-T88/T36-T50/T48-'
+		# then to 'http://www.icd10data.com/ICD10CM/Codes/S00-T88/T36-T50/T48-' + '/T48.0X4D'
+		#
+		# (the -1 on the lengths is for urls with missing '-', check M84.x)
 		if a['href'][:(len(url)-1)] == url[:(len(url)-1)] and a['href'].count('/') > url.count('/'):
+			# Go further down the tree
 			follow_links(a['href'])
+			# Don't save data
 			no_matches = False
+	# Save data if no matches
 	if no_matches:
 		icd_10_cd = url.split('/')
 		icd_10_cd = icd_10_cd[len(icd_10_cd)-1]
@@ -29,24 +47,16 @@ def follow_links(url):
 				icd10_codes[icd_10_cd] = h2.text
 		
 
-# http://www.icd10data.com/ICD10CM/Codes
+# Start at the first URL with the basic links
 url = "/ICD10CM/Codes"
-
-#url = "/ICD10CM/Codes/M00-M99/M80-M85/M84-"
-#url = "/ICD10CM/Codes/C00-D49/C7A-C7A/C7A-"
-#page = requests.get(url)
-#soup = BeautifulSoup(page.text)
-
 follow_links(url)
 
-
-#file.close()
-
+# Output all the data to a csv
 with open("ICD10_Codes.csv",'wb') as file:
 	writer = csv.writer(file)
 	for code, text in icd10_codes.items():
 		try:
-			writer.writerow([code, text.encode('latin-1')])
+			writer.writerow([code, text.encode('latin-1')]) # Watch the encoding for a few diaseses
 			print('Done')
 		except:
 			print(text)
